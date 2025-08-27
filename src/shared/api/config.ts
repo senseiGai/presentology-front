10412d2927dcb8d293d2db3aca71985170018157
senseiGai <swerss564@gmail.com>
2025-08-27 22:30:01 +0500

    fix: update API_BASE_URL to use production endpoint

--- src/shared/api/config.ts
+++ src/shared/api/config.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_BASE_URL =
+  "https://presentology-back-production.up.railway.app/";
 
 // Создаем экземпляр axios с базовой конфигурацией
 export const apiClient = axios.create({

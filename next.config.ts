c0ef4a8d39c1fad8c15a1e27959538acc269b6ef
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-08-31 21:07:14 +0500

    feat: Configure image remote patterns for uploads in Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "presentology-back-production.up.railway.app",
+        port: "",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3002",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
